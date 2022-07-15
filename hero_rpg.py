8f5a73e15850e1673a893a624c5f1aac8a07c4bf
Dan Kilian <dankilian@yahoo.com>
2022-07-15 14:48:22 -0500

    completed first draft of creation menu. starting player menu.

--- hero_rpg.py
+++ hero_rpg.py
@@ -1,5 +1,9 @@
 import pickle
 
+def save(map):
+    with open('data.pickle','wb') as filehandler:
+        pickle.dump(map.actualized_map, filehandler)
+
 
 class Map:
     def __init__(self, name, size,data):
@@ -8,8 +12,7 @@ class Map:
         self.actualized_map = data
         if self.actualized_map == []:
             self.actualized_map = self.createMap()
-            
-                
+                  
     def createMap(self):
         map = []
         for i in range(self.size):
@@ -74,9 +77,6 @@ class Map:
         newObj = PC(new_name, new_weight, new_location_row, new_location_column, new_power, new_health, new_items)
         self.actualized_map[newObj.location_row][newObj.location_column].append([newObj.name,newObj])
    
-   
-
-        
             
 #----------------------------GET LIST OF ALL THINGS------------------------------------------------------------------------------------------------------------------
 
@@ -88,10 +88,7 @@ class Map:
         all_things_in_map = []
         for every_row in range(len(self.actualized_map)):
             for every_cell in range(len(self.actualized_map[every_row])):
-                self.getListOfAllThingsInCell(self.actualized_map[every_row][every_cell], all_things_in_map)
-
-
-            
+                self.getListOfAllThingsInCell(self.actualized_map[every_row][every_cell], all_things_in_map)      
         return all_things_in_map
     
     ########################## PRINT ALL THINGS ###########################################
@@ -102,11 +99,9 @@ class Map:
             print(every_item.__dict__.get('name'), "is at [",  every_item.__dict__.get('location_row'), ", ", every_item.__dict__.get('location_column'),']')
         
         
-        
-        
     ########################### FIND THING BY NAME #########################################
 
-    def LookupByName(self):
+    def lookUpByName(self):
         all_things = self.getListOfAllThings()
         query= input("Enter the name of the thing >>")
         for i in all_things:
@@ -115,36 +110,46 @@ class Map:
                 return i        
         
         
-        
-        
     ########################## DELETE A THING ##############################################
     # *************ISSUE******* if there are any things with the same name, this will delete the first thing with a matching name on the map. This is due to not having unique identifiers. 
     def deleteThing(self):
-        print("*Delete Thing*")
-        thing_name = self.LookupByName() #Will return the object with the name the user requested. 
+        thing_name = self.lookUpByName() #Will return the object with the name the user requested. 
         revised_cell = []
         prev_cell = self.actualized_map[thing_name.location_row][thing_name.location_column]
-        
         for i in prev_cell:
             if i[0] != thing_name.name:
                 revised_cell.append(i)
-                
         self.actualized_map[thing_name.location_row][thing_name.location_column] = revised_cell
-    
+        return thing_name
     
     
     ######################### MOVE A THING ###################################################
     
     def moveThing(self):
-        pass
+        self.printAllThings()
+        thing_object = self.deleteThing()
+        thing_object.location_row, thing_object.location_column = [int(x) for x in input('enter the location of the thing you would like to add: >>').split(',')]
+        self.actualized_map[thing_object.location_row][thing_object.location_column].append([thing_object.name,thing_object])
     
+    ######################### CHANGE ATTRIBUTE OF THING ######################################
     
-    
-    
-    
-    
-    
-    
+    def changeThing(self):
+        thing = self.lookUpByName()
+        print(thing.__dict__.keys())
+        #******************************** will break if someone enters the name or location. Map would need to be deleted. *******
+        attribute_to_change = input('what would you like to change? (Do NOT change location or name of thing.)')
+        if attribute_to_change in thing.__dict__.keys():
+            
+            print(thing.__dict__[attribute_to_change])
+            if type(thing.__dict__[attribute_to_change]) == type(10):
+                thing.__dict__[attribute_to_change] = int(input('change to? >>'))
+            elif type(thing.__dict__[attribute_to_change]) == type([]):
+                print('cannot change a list of items at this time.')
+            else:
+                thing.__dict__[attribute_to_change] = input('change to? >>')
+        else:
+            print('this is not an attribute')
+
 class Thing:
     def __init__(self, name, weight, location_row, location_column):
         self.name = name
@@ -176,20 +181,60 @@ class NPC(Character):       #Child of Character
     
 
 
-
+#----------------  MENUS  ---------------------------------------------------
 
 def displayCreationMenu():
-    creation_menu = ['add thing', 'see map', 'quit','delete map', 'find thing','delete thing', 'list all things in map', 'Move a thing']
+    creation_menu = ['add thing', 'see map', 'quit','delete map', 'find thing','delete thing', 'list all things in map', 'Move a thing', 'change a thing']
     for i in range(len(creation_menu)):
         print(i+1, creation_menu[i])
     user_choice = int(input("what would you like to do? "))
     return user_choice
 
-def save(map):
-    with open('data.pickle','wb') as filehandler:
-        pickle.dump(map.actualized_map, filehandler)
 
-################## RUNS THE GAME
+def displayInGameMenu():
+    menu = ['move', 'check status', 'save and quit']
+    for i in range(len(menu)):
+        print(i+1, menu[i])
+    user_choice = int(input("what would you like to do? "))
+    return user_choice
+
+
+
+#----------------GAMEPLAY FUNCTIONS---------------------------------------------------------------------------
+
+
+def findMainCharacter(map):
+    return map.getListOfAllThings()
+    
+
+
+
+
+
+def play(map):
+    print(map)
+    main_character = findMainCharacter(map)
+    print(main_character[0])
+    print(type(main_character[0]).__name__)
+    
+    while True:
+        user_choice = displayInGameMenu()
+        if user_choice == 1:
+            pass
+        if user_choice == 2:
+            pass
+        if user_choice == 3:
+            save(map)
+            print('Game saved. thank you for playing!')
+            break
+    
+
+
+
+
+
+
+################## STARTS THE CREATION MENU. 
 def main():
     print("welcome to the game creator! Here, you can manipulate the game map before playing.")
     
@@ -212,16 +257,24 @@ def main():
         elif user_choice == 4:
             mordor.actualized_map = mordor.createMap()
         elif user_choice == 5:
-            mordor.LookupByName()
+            mordor.lookUpByName()
         elif user_choice == 6:
+            print("*Delete Thing*")
             mordor.deleteThing()
         elif user_choice == 7:
             mordor.printAllThings()
         elif user_choice == 8:
             mordor.moveThing()
+        elif user_choice == 9:
+            mordor.changeThing()
         
+
+    print(f'Welcome to Mordor!!')
+
+    play(mordor)
+
         
-        
+
         
 try:
     with open('data.pickle', 'rb') as filehandler:
